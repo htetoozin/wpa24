b4c6aa40b6b6094deb91ad9af7b8bedb5ef2adeb
htetoozin <htetoo.zi09@gmail.com>
2016-10-02 11:38:14 +0630

    Add WPA24Pro

---
+++ wpa24pro/routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Student;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+Route::get('students', function(Request $request) {
+	$students = Student::paginate(5);
+	return $students;
+});
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
